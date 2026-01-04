92a795382dda92a989df70e1200f6247098a9df9
MarcusWarnert <mrvwarnert@gmail.com>
2026-01-04 13:44:36 -0600

    finished

--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -1,16 +1,24 @@
 package org.example;
 
 public class StringUtilities {
-    // Refer to Java Basics 1 for common string methods
 
-    // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
-    // characters
+    public static boolean shortString(String message) {
+        return message.length() < 5;
+    }
 
-    // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
+    public static char firstLetter(String string) {
+        return string.charAt(0);
+    }//This won't work if the string is empty.
 
-    // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
-    // "asparagus" (lowercase only) replaced with 4 stars: "****"
+    public static String censorAsparagus(String string) {
+        return string.replace("asparagus", "****");
+    }//This will obviously not work for ASPARAGUS
 
-    // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
-    // strings are the same length, return the first string.
+    public static String bigger(String stringOne, String stringTwo) {
+        if (stringOne.length() >= stringTwo.length()) {
+            return stringOne;
+        }
+        return stringTwo;
+    }
 }
+//All tests work.
